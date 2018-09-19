95c09f2e31e39c4a969a2384146b60a4ff7e86a9
fiorellasaro <fiorella.sfl@gmail.com>
2018-09-18 21:36:57 -0500

    Cambios en color -prueba- y cambio en estructura de seccion proyectos

--- src/controller/projects.js
+++ src/controller/projects.js
@@ -4,55 +4,55 @@ $(document).ready( () => {
     });
   }); 
 
-  $('section#TCR').find('button')[1].addEventListener('click', () => {
+  $('section#TCR').find('button')[0].addEventListener('click', () => {
     window.open("https://fiorellasaro.github.io/lim-2018-01-ux-marketplace/src/");
   });
 
-  $('section#TCR').find('button')[2].addEventListener('click', () => {
+  $('section#TCR').find('button')[1].addEventListener('click', () => {
     window.open("https://github.com/fiorellasaro/lim-2018-01-ux-marketplace");
   });
 
 
-  $('section#comunal').find('button')[1].addEventListener('click', () => {
+  $('section#comunal').find('button')[0].addEventListener('click', () => {
     window.open("https://fiorellasaro.github.io/proyecto-final-common-core/src/");
   });
 
-  $('section#comunal').find('button')[2].addEventListener('click', () => {
+  $('section#comunal').find('button')[1].addEventListener('click', () => {
     window.open("https://github.com/fiorellasaro/proyecto-final-common-core");
   });
 
 
-  $('section#foodmap').find('button')[1].addEventListener('click', () => {
+  $('section#foodmap').find('button')[0].addEventListener('click', () => {
     window.open("https://fiorellasaro.github.io/lim-2018-01-foodmap/src");
   });
 
-  $('section#foodmap').find('button')[2].addEventListener('click', () => {
+  $('section#foodmap').find('button')[1].addEventListener('click', () => {
     window.open("https://github.com/fiorellasaro/lim-2018-01-foodmap");
   });
 
 
-  $('section#socialNetwork').find('button')[1].addEventListener('click', () => {
+  $('section#socialNetwork').find('button')[0].addEventListener('click', () => {
     window.open("https://fiorellasaro.github.io/lim-2018-05-bc-core-am-socialnetwork/src/");
   });
 
-  $('section#socialNetwork').find('button')[2].addEventListener('click', () => {
+  $('section#socialNetwork').find('button')[1].addEventListener('click', () => {
     window.open("https://github.com/fiorellasaro/lim-2018-05-bc-core-am-socialnetwork");
   });
 
 
-  $('section#datadashboard').find('button')[1].addEventListener('click', () => {
+  $('section#datadashboard').find('button')[0].addEventListener('click', () => {
     window.open("https://fiorellasaro.github.io/lim-2018-05-bc-core-am-datadashboard/src/");
   });
 
-  $('section#datadashboard').find('button')[2].addEventListener('click', () => {
+  $('section#datadashboard').find('button')[1].addEventListener('click', () => {
     window.open("https://github.com/fiorellasaro/lim-2018-05-bc-core-am-datadashboard");
   });
 
 
-  $('section#caesar').find('button')[1].addEventListener('click', () => {
+  $('section#caesar').find('button')[0].addEventListener('click', () => {
     window.open("https://fiorellasaro.github.io/lim-2018-05-bc-core-am-cipher/src/");
   });
 
-  $('section#caesar').find('button')[2].addEventListener('click', () => {
+  $('section#caesar').find('button')[1].addEventListener('click', () => {
     window.open("https://github.com/fiorellasaro/lim-2018-05-bc-core-am-cipher");
   });
