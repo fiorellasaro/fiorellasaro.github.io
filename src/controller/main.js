43378dc1b0722f6e1f1bd18d2969fd745a824cfb
SANCHEZ Fiorella <fiorella.sanchez@engie.com>
2019-05-30 11:03:14 -0500

    cambio direccion index

--- src/controller/main.js
+++ src/controller/main.js
@@ -5,44 +5,44 @@ $(document).ready( () => {
 }); 
 
 document.getElementById('myProjects').addEventListener('click', () => {
- window.location.href = "view/projects.html";
+ window.location.href = "src/view/projects.html";
 }); 
 
 document.getElementById('projectsButton').addEventListener('click', () => {
-  window.location.href = "view/projects.html";
+  window.location.href = "src/view/projects.html";
  }); 
 
 
 
  document.getElementById('linkedinIcon').addEventListener('mouseover', () => {
 
-  document.getElementById('linkedinIcon').src= "view/assets/img/linkedin-w.png";
+  document.getElementById('linkedinIcon').src= "src/view/assets/img/linkedin-w.png";
 
 }); 
 
  document.getElementById('linkedinIcon').addEventListener('mouseout', () => {
 
-  document.getElementById('linkedinIcon').src= "view/assets/img/linkedin.png";
+  document.getElementById('linkedinIcon').src= "src/view/assets/img/linkedin.png";
 }); 
 
 document.getElementById('mailIcon').addEventListener('mouseover', () => {
 
-  document.getElementById('mailIcon').src= "view/assets/img/gmail-w.png";
+  document.getElementById('mailIcon').src= "src/view/assets/img/gmail-w.png";
 
 }); 
 
 document.getElementById('mailIcon').addEventListener('mouseout', () => {
 
-  document.getElementById('mailIcon').src= "view/assets/img/gmail.png";
+  document.getElementById('mailIcon').src= "src/view/assets/img/gmail.png";
 }); 
 
 document.getElementById('githubIcon').addEventListener('mouseover', () => {
 
-  document.getElementById('githubIcon').src= "view/assets/img/github-sign-w.png";
+  document.getElementById('githubIcon').src= "src/view/assets/img/github-sign-w.png";
 
 }); 
 
 document.getElementById('githubIcon').addEventListener('mouseout', () => {
 
-  document.getElementById('githubIcon').src= "view/assets/img/github-sign.png";
+  document.getElementById('githubIcon').src= "src/view/assets/img/github-sign.png";
 }); 
