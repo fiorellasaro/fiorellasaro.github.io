00d8b14eb9820266c2092c32ac5027382d689a88
fiorellasaro <fiorellasaro27@gmail.com>
2022-08-29 18:26:00 -0500

    next js config

---
+++ old/src/controller/about.js
@@ -0,0 +1,62 @@
+$(document).ready( () => {
+    $("#burguer").click( () =>{
+      $("#menuContent").slideToggle("slow"); 
+    });
+  }); 
+
+document.getElementById('cv-image').addEventListener('mouseover', () => {
+
+  document.getElementById('cv-image').src= "assets/img/file green.png";
+
+}); 
+
+document.getElementById('cv-image').addEventListener('mouseout', () => {
+
+  document.getElementById('cv-image').src= "assets/img/file white.png";
+}); 
+
+document.getElementById('linkedinIcon').addEventListener('mouseover', () => {
+
+  document.getElementById('linkedinIcon').src= "assets/img/linkedin-g.png";
+
+}); 
+
+document.getElementById('linkedinIcon').addEventListener('mouseout', () => {
+
+  document.getElementById('linkedinIcon').src= "assets/img/linkedin.png";
+}); 
+
+document.getElementById('mailIcon').addEventListener('mouseover', () => {
+
+  document.getElementById('mailIcon').src= "assets/img/gmail-g.png";
+
+}); 
+
+document.getElementById('mailIcon').addEventListener('mouseout', () => {
+
+  document.getElementById('mailIcon').src= "assets/img/gmail.png";
+}); 
+
+document.getElementById('githubIcon').addEventListener('mouseover', () => {
+
+  document.getElementById('githubIcon').src= "assets/img/github-sign-g.png";
+
+}); 
+
+document.getElementById('githubIcon').addEventListener('mouseout', () => {
+
+  document.getElementById('githubIcon').src= "assets/img/github-sign.png";
+}); 
+
+
+
+document.getElementById('cv-image').addEventListener('click', () => {
+    window.open('../view/assets/CV Fiorella Sánchez Rodríguez - ES.pdf','_blank');
+}); 
+
+document.getElementById('button').addEventListener('click', () => {
+    window.open('../view/assets/CV Fiorella Sánchez Rodríguez - ES.pdf','_blank');
+}); 
+
+
+
